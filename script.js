dd61e0dcee55e3280e83aee52be4995496ad5f27
aashish529 <aashishfartyal529@gmail.com>
2025-12-16 23:30:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
-var errormessage = "";
-var missingfield = "";
 function isEmail(email) {
     var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     return regex.test(email);
-    };
-$("#submitbutton").click(function(){
-    if($('#Email').val() == ""){
-        missingfield+="<p>Email not filled</p>";
-    }
-    if($('#phoneno').val() == ""){
-        missingfield+="<p>Phone number not filled</p>";
-    }
-    if($('#password').val() == ""){
-        missingfield+="<p>Password not filled</p>";
+};
+
+$("#submitbutton").click(function() {
+    
+    var errormessage = "";
+    var missingfield = "";
+
+    $('#error').html("");
+    $('#success').html(""); 
+
+    
+    if ($('#Email').val() == "") {
+        missingfield += "<p>Email not filled</p>";
+    } else if (isEmail($('#Email').val()) == false) {
+       
+        errormessage += "<p>Email id is not valid</p>";
     }
 
+    
+    if ($('#phoneno').val() == "") {
+        missingfield += "<p>Phone number not filled</p>";
+    } else if ($.isNumeric($("#phoneno").val()) == false) {
+        errormessage += "<p>Phone number is not valid</p>";
+    }
 
-    if(isEmail($('#Email').val()) == false){
-        errormessage +="<p>Email id is not valid</p>"
+   
+    if ($('#password').val() == "") {
+        missingfield += "<p>Password not filled</p>";
     }
-    if($.isNumeric($("#phoneno").val()) == false ){
-        errormessage += "<p>Phone number is not valid</p>"
+
+    
+    if ($("#password").val() != $("#confirmpassword").val()) {
+        errormessage += "<p>Password does not match</p>";
     }
-    if($("#password").val() != $("#confirmpassword").val()){
-        errormessage +="<p>Password does not matched</p>"
-    }
-    if(errormessage == ""){
-         $('#sucess').html("You are Registered");
-    }
-    else{
-        $('#error').html(errormessage+missingfield);
+
+    if (errormessage == "" && missingfield == "") {
+        $('#success').html("You are Registered");
+    } else {
+        $('#error').html(missingfield + errormessage);
     }
 });
