d898f39edeb13eb35a481a68f80d289241904145
aashish529 <aashishfartyal529@gmail.com>
2025-12-28 23:10:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,46 @@
 function isEmail(email) {
     var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     return regex.test(email);
-};
+}
 
 $("#submitbutton").click(function() {
     
     var errormessage = "";
     var missingfield = "";
-
-    $('#error').html("");
-    $('#success').html(""); 
+    
+   
+    $("#error").html("");
 
     
     if ($('#Email').val() == "") {
         missingfield += "<p>Email not filled</p>";
-    } else if (isEmail($('#Email').val()) == false) {
-       
-        errormessage += "<p>Email id is not valid</p>";
     }
-
-    
     if ($('#phoneno').val() == "") {
         missingfield += "<p>Phone number not filled</p>";
-    } else if ($.isNumeric($("#phoneno").val()) == false) {
-        errormessage += "<p>Phone number is not valid</p>";
     }
-
-   
     if ($('#password').val() == "") {
         missingfield += "<p>Password not filled</p>";
     }
 
     
-    if ($("#password").val() != $("#confirmpassword").val()) {
-        errormessage += "<p>Password does not match</p>";
+    if ($('#Email').val() != "" && isEmail($('#Email').val()) == false) {
+        errormessage += "<p>Email ID is not valid</p>";
+    }
+    
+    if ($('#phoneno').val() != "" && $.isNumeric($("#phoneno").val()) == false) {
+        errormessage += "<p>Phone number is not valid</p>";
+    }
+    
+    if ($("#password").val() != "" && $("#password").val() != $("#confirmpassword").val()) {
+        errormessage += "<p>Passwords do not match</p>";
     }
 
+    
     if (errormessage == "" && missingfield == "") {
-        $('#success').html("You are Registered");
+        
+        $("#error").html("<p style='color:green; font-weight:bold;'>You are Registered!</p>");
     } else {
+        
         $('#error').html(missingfield + errormessage);
     }
 });
